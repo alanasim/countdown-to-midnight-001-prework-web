ebbdfa8d7058333e0c74c567388370814139837f
alanasim <alana.simoncelli@gmail.com>
2016-02-05 23:07:28 -0500

    Countdown Solution

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
+  end
+"HAPPY NEW YEAR!"
+end
+
+countdown(3)
+
+def countdown_with_sleep(number)
+  while number > 0
+    count = number.to_s
+  puts "#{count} SECOND(S)!"
+  number -= 1
+  sleep 1
+  end
 end
